21297be61d36eb3f80f93447d07227ab8c047cbc
Caleb Logemann <logemann@iastate.edu>
2018-11-30 10:05:31 -0600

    Started code for neural network

---
+++ FinalProject/airlineRevenueManagement.py
@@ -0,0 +1,195 @@
+import numpy as np
+import gym
+from gym import spaces
+import scipy.io
+import pdb
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten
+from keras.optimizers import Adam
+
+from rl.agents.dqn import DQNAgent
+from rl.policy import BoltzmannQPolicy
+from rl.memory import SequentialMemory
+
+class armEnv(gym.Env):
+    """The abstract environment class that is used by all agents. This class has the exact
+    same API that OpenAI Gym uses so that integrating with it is trivial. In contrast to the
+    OpenAI Gym implementation, this class only defines the abstract methods without any actual
+    implementation.
+    To implement your own environment, you need to define the following methods:
+    - `step`
+    - `reset`
+    - `render`
+    - `close`
+    Refer to the [Gym documentation](https://gym.openai.com/docs/#environments).
+    """
+
+    def __init__(self, filename):
+        mat = scipy.io.loadmat(filename, squeeze_me=True)
+
+        self.reward_range = (-np.inf, np.inf)
+
+        self.action_space = spaces.Discrete(2)
+
+        self.nFareClasses = mat['nFareClasses']
+        self.capacity = mat['capacity']
+        self.classSizeMean = mat['classSizeMean']
+        self.classCancelRate = mat['classCancelRate']
+        self.totalTime = mat['totalTime']
+        self.fareClassPrices = mat['fareClassPrices']
+
+        self.nDataSets = mat['nDataSets']
+        self.dataSets = mat['dataSets']
+        self.currentDataSetIndex = 0
+        self.currentDataSet = self.dataSets[0]
+
+        self.timeIndex = 0
+        self.nTimeIndices = self.currentDataSet.shape[0]
+        self.time = self.currentDataSet[0,0]
+        # when reading in data subtract 1 so that zero indexed
+        self.nextClass = int(self.currentDataSet[0, 1] - 1)
+        self.seats = np.zeros(self.nFareClasses, dtype=int)
+
+        self.cancellations = []
+
+        self.done = False
+
+        # observation (time, nextClass, seats)
+        self.observation_space = spaces.Tuple((spaces.Box(low=0,high=self.totalTime, shape=(1,), dtype=float), spaces.Discrete(self.nFareClasses), spaces.Box(low=np.zeros(self.nFareClasses), high=np.full(self.nFareClasses, 2*self.capacity), dtype=int)))
+
+        # TODO: add storage for list of arrival times
+
+    def step(self, action):
+        """Run one timestep of the environment's dynamics.
+        Accepts an action and returns a tuple (observation, reward, done, info).
+        # Arguments
+            action (object): An action provided by the environment.
+        # Returns
+            observation (object): Agent's observation of the current environment.
+            reward (float) : Amount of reward returned after previous action.
+            done (boolean): Whether the episode has ended, in which case further step() calls will return undefined results.
+            info (dict): Contains auxiliary diagnostic information (helpful for debugging, and sometimes learning).
+        """
+        if(self.done):
+            return None
+
+        reward = 0
+
+        # if accepted add to seats
+        if(action == 1):
+            # pdb.set_trace()
+            self.seats[self.nextClass] += 1
+            reward += self.fareClassPrices[self.nextClass]
+            # check if passenger will cancel
+            cancellationTime = self.currentDataSet[self.timeIndex, 2]
+            if (cancellationTime > 0):
+                self.cancellations.append((cancellationTime, self.nextClass))
+                # sort on first index cancellation time
+                self.cancellations.sort(key= lambda elem: elem[0])
+
+        # set new time and nextClass
+        if(self.timeIndex < self.nTimeIndices - 1):
+            self.timeIndex += 1
+            self.time = self.currentDataSet[self.timeIndex, 0]
+            self.nextClass = int(self.currentDataSet[self.timeIndex, 1] - 1)
+        else:
+            self.done = True
+            self.time = self.totalTime
+            self.nextClass = -1;
+
+        # remove cancellations
+        while(len(self.cancellations) > 0 and self.cancellations[0][0] < self.time):
+            classCancelled = self.cancellations[0][1]
+            self.seats[classCancelled] -= 1
+            reward -= self.fareClassPrices[classCancelled]
+            # remove first element
+            self.cancellations.pop(0)
+
+        if (self.done):
+            # compute overbooking cost
+            reward += 0
+
+        observation = (self.time, self.nextClass, self.seats)
+        return observation, reward, self.done
+
+
+    def reset(self):
+        """
+        Resets the state of the environment and returns an initial observation.
+        # Returns
+            observation (object): The initial observation of the space. Initial reward is assumed to be 0.
+        """
+
+        if(self.currentDataSetIndex + 1 >= self.nDataSets):
+            print('No More Data Sets')
+        # load next Data Set
+        self.currentDataSetIndex += 1
+        self.currentDataSet = self.dataSets[self.currentDataSetIndex]
+
+        # reset variables
+        self.timeIndex = 0
+        self.nTimeIndices = self.currentDataSet.shape[0]
+        self.time = self.currentDataSet[0,0]
+        # when reading in data subtract 1 so that zero indexed
+        self.nextClass = int(self.currentDataSet[0, 1] - 1)
+        self.seats = np.zeros(self.nFareClasses, dtype=int)
+
+        self.cancellations = []
+
+        self.done = False
+
+        observation = (self.time, self.nextClass, self.seats)
+        return observation
+
+
+    def render(self, mode='human', close=False):
+        """Renders the environment.
+        The set of supported modes varies per environment. (And some
+        environments do not support rendering at all.)
+        # Arguments
+            mode (str): The mode to render with.
+            close (bool): Close all open renderings.
+        """
+        raise NotImplementedError()
+
+    def close(self):
+        """Override in your subclass to perform any necessary cleanup.
+        Environments will automatically close() themselves when
+        garbage collected or when the program exits.
+        """
+        raise NotImplementedError()
+
+env = armEnv("test.mat")
+nb_actions = env.action_space.n
+
+# Build model
+model = Sequential()
+model.add(Flatten(input_shape=(1,6)))
+model.add(Dense(16))
+model.add(Activation('relu'))
+model.add(Dense(16))
+model.add(Activation('relu'))
+model.add(Dense(16))
+model.add(Activation('relu'))
+model.add(Dense(nb_actions))
+model.add(Activation('linear'))
+# print(model.summary())
+
+memory = SequentialMemory(limit=50000, window_length=1)
+policy = BoltzmannQPolicy()
+dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory,
+               target_model_update=1e-2, policy=policy)
+dqn.compile(Adam(lr=1e-3), metrics=['mae'])
+
+# Okay, now it's time to learn something! We visualize the training here for show, but this
+# slows down training quite a lot. You can always safely abort the training prematurely using
+# Ctrl + C.
+pdb.set_trace()
+dqn.fit(env, nb_steps=50000)
+
+# After training is done, we save the final weights.
+dqn.save_weights('dqn_weights.h5f', overwrite=True)
+
+# Finally, evaluate our algorithm for 5 episodes.
+dqn.test(env, nb_episodes=5)
--- FinalProject/qLearningGlobalApproximation.m
+++ FinalProject/qLearningGlobalApproximation.m
@@ -1,14 +1,15 @@
-function qLearningGlobalApproximation(dataSet, learningRate, discountFactor)
+function qLearningGlobalApproximation(dataSet, learningRate, discountFactor, nFareClasses, fareClassPrices)
     n = length(dataSet);
     nFareClasses = 3;
+
     % stateVector = {time, fareClassToAdd, 
     oldState = struct();
     oldState.time = dataSet(1, 1);
     oldState.nextClass = dataSet(1, 2);
     oldState.currentlyBooked = zeros(nFareClasses,1);
-    % TODO: add list of booking times
-    % Problem when someone cancels their booking time won't get deleted
-    %oldState.bookingTimes = cell(nFareClasses,1);
+    oldState.bookingTimes = cell(nFareClasses,1);
+    % cancellations
+    % [timeOfCancellation, class, timeOfBooking]
     cancellations = [];
 
     for i = 1:n
@@ -23,7 +24,7 @@ function qLearningGlobalApproximation(dataSet, learningRate, discountFactor)
             newState.currentlyBooked(oldState.nextClass) = newState.currentlyBooked(oldState.nextClass) + 1;
             %newState.bookingTimes
             if(dataSet(i, 3) > 0)
-                cancellations = [cancellations; oldState.nextClass, dataSet(i, 3)];
+                cancellations = [cancellations; dataSet(i, 3), oldState.nextClass, oldState.time];
                 cancellations = sortrows(cancellations);
             end
             reward = 100;
@@ -34,6 +35,7 @@ function qLearningGlobalApproximation(dataSet, learningRate, discountFactor)
         while (cancellations(1, 1) < newState.time)
             classToCancel = cancellations(1,2);
             newState.currentlyBooked(classToCancel) = newState.currentlyBooked(classToCancel) - 1;
+            newState.
         end
 
         % update weights on neural network
